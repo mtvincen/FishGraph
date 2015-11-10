6070e840096ca34ccf9c881871db85aa176b93b7
rcheshire <rob.cheshire@noaa.gov>
2015-11-10 11:40:52 -0500

    update package help and go.r
    modify order of go.r to match tech memo, edits for package help and put
    function descriptions in alphabetical order

--- inst/demo/go.r
+++ inst/demo/go.r
@@ -23,47 +23,28 @@ windows(width = 8, height = 8, record = TRUE)
 
 ########## Call the functions #########################
 
-Parm.plots(gag, graphics.type=ptype)
-
-Bound.vec.plots(gag, draft=dtype, graphics.type=ptype)
-
-Landings.plots(gag, draft=dtype, use.color=ctype, graphics.type=ptype, 
-        L.units=c("1000 lb gutted","1000 lb gutted","1000 fish","1000 fish"), D.units="1000 dead fish", L.obs.pre=FALSE)
-
-Index.plots(gag, draft=dtype, use.color=ctype, graphics.type=ptype)
-
+BSR.time.plots(gag, start.drop = 10, draft=dtype, use.color=ctype, graphics.type=ptype, legend.pos="top")
 
 #windows(width = 10, height = 8, record = TRUE)
 Comp.plots(gag, draft=dtype, use.color=ctype, graphics.type=ptype, corr=T, c.min=0.2)
 
 #windows(width = 8, height = 10, record = TRUE)
 Comp.yearly.plots(gag, draft=dtype, use.color=ctype, graphics.type=ptype, plot.neff=FALSE, print.neff=FALSE,
-                  compact = TRUE, print.n=TRUE)
+    compact = TRUE, print.n=TRUE)
 
 Cohort.plots(gag,draft=dtype, graphics.type=ptype)
 
-Growth.plots(gag, draft=dtype, use.color=ctype, graphics.type=ptype, plot.all = TRUE)
-
-StockRec.plots(gag, draft=dtype, use.color=ctype, graphics.type=ptype, 
-               draw.lowess = FALSE, start.drop = 10, units.rec="number age-1 fish")
+Index.plots(gag, draft=dtype, use.color=ctype, graphics.type=ptype)
 
 gag$parms$F40=0.54 #add to parms, calculated post-assessment
 gag$parms$F50=0.38 #add to parms, calculated post-assessment
 F.time.plots(gag, draft=dtype, use.color=ctype, graphics.type=ptype, 
              F.references=list(a="F40",b=c("Fmsy","F50")))
 
-#windows(width = 6, height = 4, record = TRUE, xpos = 10, ypos = 10)
-Selectivity.plots(gag, draft=dtype, use.color=ctype, graphics.type=ptype, plot.points=T)
+Landings.plots(gag, draft=dtype, use.color=ctype, graphics.type=ptype, 
+               L.units=c("1000 lb gutted","1000 lb gutted","1000 fish","1000 fish"), D.units="1000 dead fish", L.obs.pre=FALSE)
 
-
-NFZ.age.plots(gag,draft=dtype, use.color=ctype, graphics.type=ptype, 
-              user.plots="N.age.mdyr", start.drop=10)
-
-CLD.total.plots(gag, draft=dtype, use.color=ctype, graphics.type=ptype, first.year = "1965",
-                units.CLD.w="1000 lb gutted", CLD.w.references=list(NULL,"msy.klb", NULL),
-                plot.proportion = TRUE)
-
-BSR.time.plots(gag, start.drop = 10, draft=dtype, use.color=ctype, graphics.type=ptype, legend.pos="top")
+Growth.plots(gag, draft=dtype, use.color=ctype, graphics.type=ptype, plot.all = TRUE)
 
 #windows(width = 8, height = 6, record = TRUE)
 PerRec.plots(gag, draft=dtype, use.color=ctype, graphics.type=ptype, 
@@ -72,10 +53,25 @@ PerRec.plots(gag, draft=dtype, use.color=ctype, graphics.type=ptype,
 Eq.plots(gag, draft=dtype, use.color=ctype, graphics.type=ptype, 
          F.references=list("Fmsy", "F40"))    
 
+StockRec.plots(gag, draft=dtype, use.color=ctype, graphics.type=ptype, 
+               draw.lowess = FALSE, start.drop = 10, units.rec="number age-1 fish")
+
+#windows(width = 6, height = 4, record = TRUE, xpos = 10, ypos = 10)
+Selectivity.plots(gag, draft=dtype, use.color=ctype, graphics.type=ptype, plot.points=T)
+
+NFZ.age.plots(gag,draft=dtype, use.color=ctype, graphics.type=ptype, 
+              user.plots="N.age.mdyr", start.drop=10)
+
+CLD.total.plots(gag, draft=dtype, use.color=ctype, graphics.type=ptype, first.year = "1965",
+                units.CLD.w="1000 lb gutted", CLD.w.references=list(NULL,"msy.klb", NULL),
+                plot.proportion = TRUE)
+
 #windows(width = 8, height = 8, record = TRUE)
 Phase.plots(gag, start.drop=10, draft=dtype, use.color=ctype, graphics.type=ptype, year.pos=2)
 
+Parm.plots(gag, graphics.type=ptype)
 
+Bound.vec.plots(gag, draft=dtype, graphics.type=ptype)
 
 ####### object into an ASCII file (for spreadsheet users, etc.)
 #rdat2ascii(gag)
